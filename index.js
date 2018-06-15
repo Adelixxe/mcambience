0c3a479c0059aedd5beec0e2ab17e4f000759e5f
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2018-06-15 19:35:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client(); 
+const path = require('path');
+const prefix = "$";
+const fs = require('fs');
+const ddiff = require('return-deep-diff');
+const ytdl = require('ytdl-core');
+
+var cli = new Discord.Client({autoReconnect:true});
+
+bot.commands = new Discord.Collection();
+
+const url = 'https://youtube.com/watch?v=_sI_Ps7JSEk'
+
+bot.on('ready', () => {
+    console.log('McAmbience is ready');
+});
+
+bot.on('ready', () => {
+    bot.user.setPresence(({ game: { name: " Jazz Lounge", type: 2}}));
+});
+
+bot.on("message", function (message) {
+    if (message.author.equals(bot.user)) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content.startsWith(prefix)) {
+        message.delete(100)
+    }
+});
+bot.on('message', message => {
+    if (message.content.startsWith('$jazzamb')) {
+        console.log('Got a song request!');
+        const voiceChannel = message.member.voiceChannel;
+
+        if (!voiceChannel) return message.reply('Please be in a voice channel first!');
+
+        voiceChannel.join()
+
+        .then (connection => {
+            const stream = message.guild.voiceConnection.playStream(ytdl(url, { filter: 'audioonly' }))
+            music();
+        })
+
+        function music() {
+            const stream = Connection.playStream(stream)
+            .once('end', () => music());
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN);
 
